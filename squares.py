9d37974fbd398766af1fb65200c52dae0fada572
Shisei <120374041+shisei12uni@users.noreply.github.com>
2025-09-17 19:12:03 +0100

    what I wrote for issue #28

--- squares.py
+++ squares.py
@@ -1,5 +1,6 @@
 """Computation of weighted average of squares."""
 
+from argparse import ArgumentParser
 
 def average_of_squares(list_of_numbers, list_of_weights=None):
     """ Return the weighted average of the square of a list of values.
@@ -51,11 +52,13 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
-    weight_strings = ["1","1","1"]        
-    
-    numbers = convert_numbers(numbers_strings)
-    weights = convert_numbers(weight_strings)
+    parser = ArgumentParser(description="Weighted average of the square of a list of values.")
+    parser.add_argument("numbers")
+    parser.add_argument("weights", action="store_true")
+    arguments= parser.parse_args()  
+
+    numbers = arguments.numbers
+    weights = arguments.weights      
     
     result = average_of_squares(numbers, weights)
     
